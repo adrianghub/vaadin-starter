1a9280de172397c7e67a7605fec3a375113aa54c
Adrian <adrian_zinko@op.pl>
2021-10-25 23:40:19 +0200

    Style login form

--- src/main/java/com/kodilla/project/xpanser/ui/login/LoginView.java
+++ src/main/java/com/kodilla/project/xpanser/ui/login/LoginView.java
@@ -3,6 +3,8 @@ package com.kodilla.project.xpanser.ui.login;
 import com.kodilla.project.xpanser.backend.service.AuthService;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -10,10 +12,11 @@ import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.router.RouterLink;
 
 @Route("login")
 @PageTitle("Login | Xpanser")
+@JsModule("./styles/shared-styles.js")
+@CssImport("./styles/views/login/login-view.css")
 public class LoginView extends VerticalLayout {
 
     public LoginView(AuthService authService) {
